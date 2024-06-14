73eda5aa3ce66fb4f959cf61d65d9b5f40518c4b
PyPiSan <sanjeev.mishra96@gmail.com>
2024-06-14 23:33:48 +0530

    first commit

---
+++ src/main/java/com/java/socket/io/pypisan/controllers/SocketController.java
@@ -0,0 +1,53 @@
+package com.java.socket.io.pypisan.controllers;
+
+
+import com.corundumstudio.socketio.AckRequest;
+import com.corundumstudio.socketio.SocketIOClient;
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.listener.ConnectListener;
+import com.corundumstudio.socketio.listener.DataListener;
+import com.corundumstudio.socketio.listener.DisconnectListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import lombok.extern.log4j.Log4j2;
+
+
+@Component
+@Log4j2
+public class SocketController {
+
+    @Autowired
+    private SocketIOServer socketServer;
+
+    SocketController(SocketIOServer socketServer){
+        this.socketServer=socketServer;
+
+        this.socketServer.addConnectListener(onUserConnectWithSocket);
+        this.socketServer.addDisconnectListener(onUserDisconnectWithSocket);
+
+        /**
+         * Here we create only one event listener
+         * but we can create any number of listener
+         * messageSendToUser is socket end point after socket connection user have to send message payload on messageSendToUser event
+         */
+        // this.socketServer.addEventListener("messageSendToUser", Message.class, onSendMessage);
+
+    }
+
+
+    public ConnectListener onUserConnectWithSocket = new ConnectListener() {
+        @Override
+        public void onConnect(SocketIOClient client) {
+            log.info("Perform operation on user connect in controller");
+        }
+    };
+
+
+    public DisconnectListener onUserDisconnectWithSocket = new DisconnectListener() {
+        @Override
+        public void onDisconnect(SocketIOClient client) {
+            log.info("Perform operation on user disconnect in controller");
+        }
+    };
+    
+}
